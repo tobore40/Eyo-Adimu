eb61a11c8bb9f3f4f64474d8bfa37803c7af6ff2
tobore40 <tobore40@gmail.com>
2021-09-21 14:13:48 +0100

    eyo

---
+++ script.js
@@ -0,0 +1,130 @@
+const myslide = document.querySelectorAll('.myslide'),
+	  dot = document.querySelectorAll('.dot');
+let counter = 1;
+slidefun(counter);
+
+let timer = setInterval(autoSlide, 8000);
+function autoSlide() {
+	counter += 1;
+	slidefun(counter);
+}
+function plusSlides(n) {
+	counter += n;
+	slidefun(counter);
+	resetTimer();
+}
+function currentSlide(n) {
+	counter = n;
+	slidefun(counter);
+	resetTimer();
+}
+function resetTimer() {
+	clearInterval(timer);
+	timer = setInterval(autoSlide, 8000);
+}
+
+function slidefun(n) {
+	
+	let i;
+	for(i = 0;i<myslide.length;i++){
+		myslide[i].style.display = "none";
+	}
+	for(i = 0;i<dot.length;i++) {
+		dot[i].className = dot[i].className.replace(' active', '');
+	}
+	if(n > myslide.length){
+	   counter = 1;
+	   }
+	if(n < 1){
+	   counter = myslide.length;
+	   }
+	myslide[counter - 1].style.display = "block";
+	dot[counter - 1].className += " active";
+}
+
+// STICKY NAVBAR
+
+let height = $('#top').height();
+$(window).scroll(function () {
+	if ($(this).scrollTop() > height) {
+	$('#navbar').addClass('fixed');	
+	} else {
+		$('#navbar').removeClass('fixed');
+	}
+});
+
+// GRID VIEW
+
+var li_links = document.querySelectorAll(".links ul li");
+var view_wraps = document.querySelectorAll(".view_wrap");
+var list_view = document.querySelector(".list-view");
+var grid_view = document.querySelector(".grid-view");
+
+li_links.forEach(function(link){
+	link.addEventListener("click", function(){
+		li_links.forEach(function(link){
+			link.classList.remove("active");
+		})
+
+		link.classList.add("active");
+
+		var li_view = link.getAttribute("data-view");
+
+		view_wraps.forEach(function(view){
+			view.style.display = "none";
+		})
+
+		if(li_view == "list-view"){
+			list_view.style.display = "block";
+		}
+		else{
+			grid_view.style.display = "block";
+		}
+	})
+})
+
+
+
+$(document).ready(function() {
+  $(".menu-toggle").on("click", function() {
+    $(".nav").toggleClass("showing");
+    $(".nav ul").toggleClass("showing");
+  });
+
+  $(".post-wrapper").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    nextArrow: $(".next"),
+    prevArrow: $(".prev"),
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+});
